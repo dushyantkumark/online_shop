8b75a1f6889eb1e164060577d3650b7e9bcbf31e
Dushyant Kumar <116090315+dushyantkumark@users.noreply.github.com>
2025-03-02 17:03:21 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  css: {
+    devSourcemap: false
+  },
+  server: {
+    allowedHosts: ['my.awscloudshop.online']
+  }
+})
+
+/*
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -9,3 +25,4 @@ export default defineConfig({
       devSourcemap: false
     }
 })
+*/
