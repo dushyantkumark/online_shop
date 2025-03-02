d58d6aa91668e9b2226095a71dc1f013e86a1c38
Dushyant Kumar <116090315+dushyantkumark@users.noreply.github.com>
2025-03-02 16:14:48 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 set -e
 
-# Load environment variables with decryption for SecureString parameters
+# Load environment variables from AWS Parameter Store
 AWS_ACCOUNT_ID=$(aws ssm get-parameter --name "/myapp/aws-account-id" --with-decryption --query "Parameter.Value" --output text)
 AWS_REGION=$(aws ssm get-parameter --name "/myapp/aws-region" --with-decryption --query "Parameter.Value" --output text)
 ECR_REPOSITORY_NAME=$(aws ssm get-parameter --name "/myapp/ecr-repository/name" --with-decryption --query "Parameter.Value" --output text)
 
 REPOSITORY_URI="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPOSITORY_NAME"
 
-# Ensure IMAGE_TAG is provided
-if [ -z "$IMAGE_TAG" ]; then
-  echo "Error: IMAGE_TAG environment variable is not set."
+# Load IMAGE_TAG from file
+if [ -f "/home/ubuntu/online-shop-app/imageTag.txt" ]; then
+  IMAGE_TAG=$(cat /home/ubuntu/online-shop-app/imageTag.txt)
+else
+  echo "Error: IMAGE_TAG file not found!"
   exit 1
 fi
 
@@ -20,17 +22,17 @@ echo "Using Image Tag: $IMAGE_TAG"
 echo "Logging in to Amazon ECR..."
 aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin "$REPOSITORY_URI"
 
-# Pulling the image with the dynamic tag
+# Pulling the image from ECR
 echo "Pulling image from ECR with tag: $IMAGE_TAG..."
 docker pull "$REPOSITORY_URI:$IMAGE_TAG"
 
-# Stopping existing container if running
+# Stop existing container if running
 EXISTING_CONTAINER=$(docker ps -q --filter "name=online-shop-container")
 if [ -n "$EXISTING_CONTAINER" ]; then
   echo "Stopping and removing existing container..."
   docker stop "$EXISTING_CONTAINER" && docker rm "$EXISTING_CONTAINER"
 fi
 
-# Starting the container
+# Starting new container
 echo "Starting new container..."
 docker run -d -p 80:5173 --name online-shop-container "$REPOSITORY_URI:$IMAGE_TAG"
