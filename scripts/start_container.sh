8e44dde4f9246caeeb598e980d9e2c03cf77fbfb
Dushyant Kumar <116090315+dushyantkumark@users.noreply.github.com>
2025-03-02 15:16:47 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -7,13 +7,18 @@ AWS_REGION=$(aws ssm get-parameter --name "/myapp/aws-region" --query "Parameter
 ECR_REPOSITORY_NAME=$(aws ssm get-parameter --name "/myapp/ecr-repository/name" --query "Parameter.Value" --output text)
 
 REPOSITORY_URI="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPOSITORY_NAME"
-IMAGE_TAG="latest"
 
+# Assuming IMAGE_TAG is passed as an environment variable or set by CodePipeline
+echo "Using Image Tag: $IMAGE_TAG"
+
+# Logging in to Amazon ECR
 echo "Logging in to Amazon ECR..."
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $REPOSITORY_URI
 
-echo "Pulling latest image from ECR..."
+# Pulling the image with the dynamic tag
+echo "Pulling image from ECR with tag: $IMAGE_TAG..."
 docker pull $REPOSITORY_URI:$IMAGE_TAG
 
+# Starting the container
 echo "Starting container..."
 docker run -d -p 80:5173 --name online-shop-container $REPOSITORY_URI:$IMAGE_TAG
