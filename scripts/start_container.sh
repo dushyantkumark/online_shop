2ca829a2c60169c5220428dac92dba66900b3859
Dushyant Kumar <116090315+dushyantkumark@users.noreply.github.com>
2025-03-02 14:42:50 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,8 +1,19 @@
-  #!/bin/bash
-  set -e
+#!/bin/bash
+set -e
 
-  # Pull the latest Docker image from Docker Hub
-  docker pull trainwithshubham/tws-online-shop-app-demo:latest
+# Load environment variables
+AWS_ACCOUNT_ID=$(aws ssm get-parameter --name "/myapp/aws-account-id" --query "Parameter.Value" --output text)
+AWS_REGION=$(aws ssm get-parameter --name "/myapp/aws-region" --query "Parameter.Value" --output text)
+ECR_REPOSITORY_NAME=$(aws ssm get-parameter --name "/myapp/ecr-repository/name" --query "Parameter.Value" --output text)
 
-  # Run the Docker image as a container
-  docker run -d -p 5173:5173 trainwithshubham/tws-online-shop-app-demo:latest
+REPOSITORY_URI="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPOSITORY_NAME"
+IMAGE_TAG="latest"
+
+echo "Logging in to Amazon ECR..."
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $REPOSITORY_URI
+
+echo "Pulling latest image from ECR..."
+docker pull $REPOSITORY_URI:$IMAGE_TAG
+
+echo "Starting container..."
+docker run -d -p 5173:5173 --name online-shop-container $REPOSITORY_URI:$IMAGE_TAG
